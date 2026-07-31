386e332234666f0b60206b3dc35eb6500c9da4bd
ASSOGBA-joel <gjoelassogba@gmail.com>
2026-07-31 10:19:28 +0200

    Update mail.php

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -37,7 +37,7 @@ return [
             'transport' => 'smtp',
             'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'host' => env('MAIL_HOST', 'smtp.resend.com'),
             'port' => env('MAIL_PORT', 587),
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
