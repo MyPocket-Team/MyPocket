4c8f0c85cdd2e0c8bf3df8af0e1606f72d39fa83
ASSOGBA-joel <gjoelassogba@gmail.com>
2026-08-04 17:00:38 +0200

    Update TransactionResource.php

--- backend/app/Http/Resources/TransactionResource.php
+++ backend/app/Http/Resources/TransactionResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class TransactionResource extends JsonResource
 {
@@ -14,7 +15,14 @@ class TransactionResource extends JsonResource
             'categorie' => new CategorieResource($this->whenLoaded('categorie')),
             'montant' => $this->montant,
             'description' => $this->description,
-            'date_transaction' => $this->date_transaction,
+            // Correction Problème 7 : Formatage propre "YYYY-MM-DD à HH:mm:ss"
+            'date_transaction' => $this->date_transaction 
+                ? Carbon::parse($this->date_transaction)->format('Y-m-d \à H:i:s') 
+                : null,
+            // Correction Problème 7 : Ajout de la date brute (YYYY-MM-DD) pour faciliter le regroupement côté Vue.js
+            'date_brute' => $this->date_transaction 
+                ? Carbon::parse($this->date_transaction)->format('Y-m-d') 
+                : null,
             'solde_avant' => $this->solde_avant,
             'solde_apres' => $this->solde_apres,
             'type' => $this->type,
